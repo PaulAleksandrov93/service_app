864ae533273089c55b475b20ce48d43532b47a1b
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-08-06 17:25:35 +0500

    using Celery task for optimization#2

--- service/services/tasks.py
+++ service/services/tasks.py
@@ -1,12 +1,41 @@
+import datetime
+import time
+
 from celery import shared_task
+from celery_singleton import Singleton
+from django.db import transaction
 from django.db.models import F
 
-@shared_task
+
+@shared_task()
 def set_price(subscription_id):
     from services.models import Subscription
 
-    subscription = Subscription.objects.filter(id=subscription_id).annotate(
-        annotated_price=F('service__full_price') -
-              F('service__full_price') * F('plan__discount_percents') / 100.00).first()
-    subscription.price = subscription.annotated_price
-    subscription.save()
+    with transaction.atomic():
+        time.sleep(5)
+
+        subscription = Subscription.objects.select_for_update().filter(id=subscription_id).annotate(
+            annotated_price=F('service__full_price') -
+                  F('service__full_price') * F('plan__discount_percents') / 100.00).first()
+
+        time.sleep(20)
+
+        subscription.price = subscription.annotated_price
+        subscription.save()
+
+    print('do something')
+
+@shared_task(base=Singleton)
+def set_comment(subscription_id):
+    from services.models import Subscription
+
+    print('do something 1')
+    with transaction.atomic():
+        subscription = Subscription.objects.select_for_update().get(id=subscription_id)
+
+        time.sleep(27)
+
+        subscription.comment = str(datetime.datetime.now())
+        subscription.save()
+
+    print('do something 2')
